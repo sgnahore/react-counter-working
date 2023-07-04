23ddbed863dba8bfb73d4a89e9a140c016337134
Sgnahore <stephaniergnahore@gmail.com>
2023-07-04 16:13:00 +0100

    Reset and Multiple buttons added

--- src/components/CounterDemo.tsx
+++ src/components/CounterDemo.tsx
@@ -8,15 +8,33 @@ function CounterDemo(): JSX.Element {
     queueRerenderWithNewCounterValue(counterValueFromCurrentRender + 1);
   };
 
+  const handleMinusFive = () => {
+    queueRerenderWithNewCounterValue(counterValueFromCurrentRender - 5);
+  };
+
+  const handleMinusOne = () => {
+    queueRerenderWithNewCounterValue(counterValueFromCurrentRender - 1);
+  };
+
+  const handlePlusFive = () => {
+    queueRerenderWithNewCounterValue(counterValueFromCurrentRender + 5);
+  };
   const handleReset = () => {
+    queueRerenderWithNewCounterValue(
+      counterValueFromCurrentRender - counterValueFromCurrentRender
+    );
+
     // TODO reset counter to 0 here
-  }
+  };
 
   return (
     <>
       <h1>Counter Demo</h1>
       <p>Current value: {counterValueFromCurrentRender}</p>
       <button onClick={handleAddOne}>+1</button>
+      <button onClick={handleMinusFive}>-5</button>
+      <button onClick={handleMinusOne}>-1</button>
+      <button onClick={handlePlusFive}>+5</button>
       <hr />
       <button onClick={handleReset}>Reset counter</button>
     </>
